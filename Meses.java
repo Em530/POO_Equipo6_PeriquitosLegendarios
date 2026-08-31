6053d92078445e3bdfd7a08024db48f0df93069c
Em530 <110864328+Em530@users.noreply.github.com>
2026-08-30 19:14:54 -0600

    Add program to determine month and season
    The program allows the user to input a month number and provides the corresponding month name and season. It also prompts for a day in certain months to determine the exact season.

---
+++ Meses.java
@@ -0,0 +1,120 @@
+
+/**
+ * Programa para saber la estacion del año con respecto
+ *  al mes seleccionado por el usuario.
+ */
+
+import java.util.*;
+
+public class Meses{
+    public static void main (String[] args){
+
+        //Leer datos del teclado.
+        Scanner teclado = new Scanner(System.in);
+        int valor1;
+
+        System.out.println("\n\t\t ------Meses del Año------");
+
+        System.out.print("\n\t\nProporcione el número del mes deseado (1 - 12): ");
+        valor1 = teclado.nextInt(); //Guarda el dato seleccionado por el usuario.
+
+        /**
+         * Se usa la sentencia "swith" para que cada caso sea un mes.
+         * 
+         * @return El mes seleccionado y la estacion, en casos especiales
+         *          se pedira el dia y dependiendo del dia se regresara
+         *           el mes y la estacion correspondiente.
+         */
+
+        switch (valor1){
+            case 1:
+                System.out.println("\n\tEl mes seleccionado es ENERO y es Invierno");
+                break;
+
+            case 2:
+                System.out.println("\n\tEl mes seleccionado es FEBRERO y es Invierno");
+                break;
+
+            case 3:
+                System.out.println("\n\tEl mes seleccionado es MARZO");
+                System.out.print("\n\tProporcione un día del mes seleccionado (1 - 31): ");
+                valor1 = teclado.nextInt();
+                
+                //Pide un dia especifico al usuario y lo compara para dar la estacion exacta. 
+                if(valor1 < 21){
+                    System.out.println("\n\tEs MARZO y es Invierno");
+                }
+                else if(valor1 >= 21 && valor1 <= 31){
+                    System.out.println("\n\tEs MARZO y es Primavera");
+                }
+                
+                break;
+
+            case 4:
+                System.out.println("\n\tEl mes seleccionado es ABRIL y es Primavera");
+                break;
+
+            case 5:
+                System.out.println("\n\tEl mes seleccionado es MAYO y es Primavera");
+                break;
+
+            case 6:
+                System.out.println("\n\tEl mes seleccionado es JUNIO");
+                System.out.print("\n\tProporcione un día del mes seleccionado: ");
+                valor1 = teclado.nextInt();
+                
+                if(valor1 < 21){
+                    System.out.println("\n\tEs JUNIO y es Primavera");
+                }
+                else if(valor1 >= 21 && valor1 < 30){
+                    System.out.println("\n\tEs JUNIO y es Verano");
+                }
+                break;
+            case 7:
+                System.out.println("\n\tEl mes seleccionado es JULIO y es Verano");
+                break;
+
+            case 8:
+                System.out.println("\n\tEl mes seleccionado es AGOSTO y es Verano");
+                break;
+
+            case 9:
+                System.out.println("\n\tEl mes seleccionado es Septiembre");
+                System.out.print("\n\tProporciones un día del mes seleccionado: ");
+                valor1 = teclado.nextInt();
+
+                if(valor1 < 21){
+                    System.out.println("\n\tEs SEPTIEMBRE y es Verano");
+                }
+                else if(valor1 >= 21 && valor1 < 30){
+                    System.out.println("\n\tEs SEPTIEMBRE y es Otoño");
+                }
+                break;
+
+            case 10:
+                System.out.println("\n\tEl mes seleccionado es OCTUBRE y es Otoño");
+                break;
+
+            case 11:
+                System.out.println("\n\tEl mes seleccionado es NOVIEMBRE y es Otoño");
+                break;
+
+            case 12:
+                System.out.println("\n\tEl mes seleccionado es DICIEMBRE");
+                System.out.print("\nProporcione un dia del mes seleccionado(1 - 31): ");
+                valor1 = teclado.nextInt();
+
+                if(valor1 < 21){
+                    System.out.println("\n\tEl mes seleccionado es DICIEMBRE y es Otoño");
+                }
+                else if(valor1 >= 21 && valor1 < 31){
+                    System.out.println("\n\tEl mes seleccionado es DICIEMBRE y es Invierno");
+                }
+                break;
+
+            default:
+                System.out.println("\n\tEl mes seleccionado NO ES VALIDO");
+                break;
+        }
+    }
+}
