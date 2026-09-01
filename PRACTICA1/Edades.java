032ae634166c190af229f8824c26d07f3cde87d5
natczf <natydelcruz@gmail.com>
2026-09-01 10:56:16 -0600

    Close Scanner object in Edades.java
    Close the Scanner object to free resources.

--- PRACTICA1/Edades.java
+++ PRACTICA1/Edades.java
@@ -28,6 +28,7 @@ public class Edades {
          } else { //si el usuario escribio un valor diferente a int se imprime el mensaje de error
             System.out.println("Ups, debes ingresar un numero entero valido:)");
           } 
+         leer.close();//Cerramos el objeto de la clase Scanner para liberar recursos.
    }
 } 
        
