dd6711c0d3418abb52697c51f6a9c6f4bd4fbaab
natczf <natydelcruz@gmail.com>
2026-09-08 11:20:11 -0600

    Refactor Main.java for clarity and comments

--- PRACTICA2/Main.java
+++ PRACTICA2/Main.java
@@ -1,24 +1,25 @@
-/** Práctica 1:Fundamentos y sintaxis del lenguaje
+ /** Práctica 1:Fundamentos y sintaxis del lenguaje
  * main del programa para realizar el registro de alumnos
  **/
 
-import java.util.Scanner;
+import java.util.Scanner; //Biblioteca para leer datos de entrada
 
-public class Main {
-    public static void main(String[] args) {
-        
-    Alumno [] alumnos = new Alumno[10];
+public class Main { //Clase principal del programa
+    public static void main(String[] args) {//Método principal del programa
 
+    Alumno [] alumnos = new Alumno[10];//Arreglo de objetos Alumno
+        //Se crean dos objetos Alumno con datos predefinidos
     alumnos[0]  = new Alumno(20, 8.5f, "Pedro", "3er Semestre", "H");
     alumnos[1]  = new Alumno(22, 9.2f, "Ana", "6to Semestre", "M");
+    
     Scanner sc = new Scanner(System.in);
-
+        //Variables para almacenar los datos ingresados por el usuario
     int edad;
     float promedio;
     String nombre, semestre;
     int i=2;
-    while(i<10){
-        
+    while(i<10){//Ciclo para registrar los datos de los alumnos
+        System.out.println("  --REGISTRO DE ALUMNOS--");
         alumnos[i] = new Alumno();
 
         System.out.println("Por favor proporcione el nombre del alumno: ");
@@ -30,7 +31,7 @@ public class Main {
         semestre = sc.nextLine();
         alumnos[i].setSemestre(semestre);
         //sc.next();
-
+        //Validación de la edad del alumno
         boolean edadValida = false;
         while(!edadValida) {
         System.out.println("Por favor proporcione la edad del alumno: ");
@@ -43,7 +44,7 @@ public class Main {
             }
         } 
         
-
+        //Validación del promedio del alumno
         boolean promedioValido = false;
         while(!promedioValido){
         System.out.println("Por favor proporcione el promedio del alumno: ");
@@ -55,7 +56,7 @@ public class Main {
             System.out.println("Promedio no valido, por favor ingrese un promedio entre 0.0 y 10.0");
             }
         }
-
+        //Validación del sexo del alumno
         boolean sexoValido = false;
         while(!sexoValido){
         System.out.println("Por favor proporcione el sexo(H/h o M/m): ");
@@ -74,7 +75,7 @@ public class Main {
 
         i++;
     }
-
+    //Mostrar la lista de alumnos registrados
     System.out.println("  LISTA DE ALUMNOS:  ");
     for (int j=0; j<i; j++){
         System.out.println("Nombre: "+alumnos[j].getNombre());
@@ -84,6 +85,6 @@ public class Main {
         System.out.println("Sexo: "+alumnos[j].getSexo());
     }
     
-    sc.close();
+    sc.close();//Cierre del objeto Scanner para liberar recursos
     }
 }
