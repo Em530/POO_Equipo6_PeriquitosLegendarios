d721dc04ea626858f20334cfd308ab6e08fe2be5
Em530 <110864328+Em530@users.noreply.github.com>
2026-09-17 03:43:22 -0600

    Remove program description comments
    Removed comments describing the program's functionality.

--- PRACTICA3/mensajeDedicatoria.java
+++ PRACTICA3/mensajeDedicatoria.java
@@ -3,9 +3,6 @@
  * De la Cruz Flores Natalia Michelle,
  * Hernández Moreno Emiliano, 
  * Martinez Barrios Aarón Rodrigo.
- * Programa para realizar el registro de alumnos, 
- * donde se solicita al usuario ingresar los datos de cada alumno
- * y posteriormente se muestran en pantalla.
  */
 import java.util.Scanner;
 import java.util.Date;
