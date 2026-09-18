e57c6b2a19aae266dfa35d1d5fef35ab946bab4d
Em530 <110864328+Em530@users.noreply.github.com>
2026-09-17 19:45:42 -0600

    Update MensajeDedicatoria.java

--- PRACTICA3/MensajeDedicatoria.java
+++ PRACTICA3/MensajeDedicatoria.java
@@ -1,4 +1,7 @@
 /** Práctica 3: Utilerías y clases de uso general
+ * Codigo 4: El codigo recibe un nombre y fecha de nacimiento
+ * para dejar un mensaje creado por el usuario, con fecha del dia actual
+ * Se implemento el uso de StringBuffer, StringBuilder, Date y Calendar
  * Código realizado por: Corona Palacios Diego André,
  * De la Cruz Flores Natalia Michelle,
  * Hernández Moreno Emiliano, 
